75bd6ec68edc5bf183acac61e84503ed365fec82
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-14 11:32:17 -0700

    Adds tester rails application

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,37 @@
+class ProductsController < ApplicationController
+  def index
+    product = Product.all
+
+    render json: product.as_json
+  end
+
+  def show
+    product = Product.find_by id: params[:id]
+
+    render json: product.as_json
+  end
+
+  def create
+    product = Product.new(name: params[:name], description: params[:description], price: params[:price])
+
+    render json: product.as_json if product.save
+  end
+
+  def update
+    product = Product.find_by id: params[:id]
+
+    product.name = params[:name] || product.name
+    product.description = params[:description] || product.description
+    product.price = params[:price] || product.price
+
+    render json: product.as_json if product.save
+  end
+
+  def destroy
+    product = Product.find_by id: params[:id]
+
+    product.delete
+    
+    render json: {message: "Product Annihilated"} 
+  end
+end
